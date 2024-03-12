eab6ad70bbc2e0101a43bf5610d3de72ed00e494
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2024-03-12 07:31:39 -0700

    started a bit of the win script

--- FryingGolf/Assets/Scripts/WinnerText.cs
+++ FryingGolf/Assets/Scripts/WinnerText.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class WinnerText : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Text Winner;
+    private object Goal;
+
     void Start()
     {
-        
+      
+
     }
 
     // Update is called once per frame
