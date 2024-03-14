92fc7bb869efd3f9996e066853bfb9cf96115087
kjackson6 <129884531+kJackson6@users.noreply.github.com>
2024-03-14 08:40:52 -0700

    Script Creation + DeathZone Prefab
    Created:
    DeathZone Script
    PlayerCharacter Script
    Respawn Script
    SetSpawn Script
    DeathZone Prefab

---
+++ FryingGolf/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Programmer: Jackson, Katherine
+//Updated: 03/14/24
+//This script detects collision with the player object and destroys it
+
+public class DeathZone : MonoBehaviour
+{
+    private Vector3 currentPos;
+
+    private void Start()
+    {
+    }
+
+    private void DestroyPlayer()
+    {
+    }
+}
