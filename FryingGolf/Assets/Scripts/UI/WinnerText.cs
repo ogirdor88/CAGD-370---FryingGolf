aad782a905f7fa6341ac1c6c57d625ad6ed56602
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2024-03-14 18:03:04 -0700

    Goal testing#2

--- FryingGolf/Assets/Scripts/UI/WinnerText.cs
+++ FryingGolf/Assets/Scripts/UI/WinnerText.cs
@@ -12,18 +12,24 @@ public class WinnerText : MonoBehaviour
 
     void Start()
     {
-      
+        SetCountText();
+        //Try 3 
 
     }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag=="Player")
+        if(gameObject.tag=="Goalcheck")
         {
             SetCountText();
             print("Text winner appears");
             winnerText.text = "GOAL";
+            //Heres comes test 1 
+            //Test 1 :Failed No info on the text
+            //Maybe changing the gameObject.tag might fix it maybe I Dunno 
+            //That did not work wooo
+            
         }
     }
     void Update()
