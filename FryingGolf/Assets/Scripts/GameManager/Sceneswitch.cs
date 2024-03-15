b33b4c73e473f98d83d0ec6f4cef18924da30e60
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2024-03-14 19:04:37 -0700

    Scene change is in progress
    Still need to fix where it lands

--- FryingGolf/Assets/Scripts/GameManager/Sceneswitch.cs
+++ FryingGolf/Assets/Scripts/GameManager/Sceneswitch.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sceneswitch : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject Canvas;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(Player);
+        DontDestroyOnLoad(Canvas);
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+if(collision.gameObject.tag=="Player")
+        {
+            SceneSwitch( 1);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SceneSwitch(int level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
