b0083c2c72a9a7a5bfe1a14086c421c626b0de1e
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2024-03-14 17:13:22 -0700

    Attempt #1 Goal check

--- FryingGolf/Assets/Scripts/UI/WinnerText.cs
+++ FryingGolf/Assets/Scripts/UI/WinnerText.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 public class WinnerText : MonoBehaviour
 {
+    //This script is planned for when the player 
     // Start is called before the first frame update
-    private Text Winner;
-    private object Goal;
+    private Text winnerText;
+    public GameObject Goal;
+    private int goal;
 
     void Start()
     {
@@ -15,8 +17,22 @@ public class WinnerText : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            SetCountText();
+            print("Text winner appears");
+            winnerText.text = "GOAL";
+        }
+    }
     void Update()
     {
         
+    }
+    private void SetCountText()
+    {
+        winnerText.text = "goal" + goal.ToString();
+
     }
 }
