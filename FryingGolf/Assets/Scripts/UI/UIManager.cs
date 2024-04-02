e1e5a710c1aab77642b83623517ad403c0892f88
Katherine Jackson <129884531+kJackson6@users.noreply.github.com>
2024-04-02 09:49:50 -0700

    UI Script, Level 1 fix, New Material
    New UI Script
    Fixed Level 1
    Added ground material
    Added Environment folder to art --> materials

---
+++ FryingGolf/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author(s): Jackson, Katherine
+//Updated: 04/02/24
+//This script executes all code relevant to the User Interface.
+
+public class UIManager : MonoBehaviour
+{
+    GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    private void Resume()
+    {
+
+    }
+}
