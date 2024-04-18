294f399ec6454540582dee1be52cc985c41609d7
Spongebobswitch <fjuribe-hernandez@csuchico.edu>
2024-04-17 19:26:14 -0700

    worked on the dialogue text
    Working now on the actual script of this cause this will not take me too long ..Hopefully

---
+++ FryingGolf/Assets/Scripts/UI/DialogueTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{//Author(s):  Uribe-Hernandez, Fernando
+ //Updated: 04/17/24
+ //This script manages dialogue trigger boxes
+
+    // Start is called before the first frame update
+    public GameObject Dialogue;
+
+    void Start()
+    {
+        Dialogue.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.tag=="Player")
+        {
+            Dialogue.SetActive(true);
+            Invoke("disapear", 6f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        Dialogue.SetActive(false);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void disapear()
+    {
+        Dialogue.SetActive(false);
+    }
+}
