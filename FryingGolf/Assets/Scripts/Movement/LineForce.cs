02c7fb2770795f6e8b3568f81b8704cd19aa2263
kjackson6 <129884531+kJackson6@users.noreply.github.com>
2024-04-19 14:48:09 -0700

    LineForce.Script Updated
    _isIdle bool is now public

--- FryingGolf/Assets/Scripts/Movement/LineForce.cs
+++ FryingGolf/Assets/Scripts/Movement/LineForce.cs
@@ -15,7 +15,7 @@ public class LineForce : MonoBehaviour
     //Updating strokeCount in function Shootball to be used in UIManager script.
     public int strokeCount;
 
-    [SerializeField] private bool _isIdle;
+    public bool _isIdle;
     private bool _isAiming;
     private Color _color;
 
