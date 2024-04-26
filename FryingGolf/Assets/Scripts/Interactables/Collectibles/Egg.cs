5b800ff3871b2789e14274bbd6b59525014bac4e
kjackson6 <129884531+kJackson6@users.noreply.github.com>
2024-04-26 15:07:50 -0700

    Egg isTrigger Automate
    Egg Script attached to Sunny-Side Up Egg automates setting isTrigger on egg's box collider to True upon start.
    
    Basically, just place the eggs and the isTrigger will update when the game is started.

--- FryingGolf/Assets/Scripts/Interactables/Collectibles/Egg.cs
+++ FryingGolf/Assets/Scripts/Interactables/Collectibles/Egg.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Author(s): Jackson, Katherine
+//Updated: 04/26/24
+//This script sets the isTrigger component attached to the GameObject to true upon start.
+
 public class Egg : MonoBehaviour
 {
     BoxCollider m_boxCollider;
 
-    private void Awake()
+    private void Start()
     {
-        //GameObject.GetComponent<m_boxCollider.isTrigger>();
+        //Fetch GameObject's collider
+        m_boxCollider = GetComponent<BoxCollider>();
+        //Sets the Collider's isTrigger to true
+        m_boxCollider.isTrigger = true;
+        //Output whether the Collider is a trigger type Collider or not
+        Debug.Log("Trigger On : " + m_boxCollider.isTrigger);
     }
 }
